01ffe4854e9670bd6a7708b97750afc5f84e3e8d
farhan mualif <paam6765@gmail.com>
2023-07-12 11:29:32 +0700

    payment

--- public/js/script.js
+++ public/js/script.js
@@ -8,33 +8,56 @@ $(document).ready(function(){
   });
 });
 
+
 const confirmDelete = (title) => {
   return confirm(`Yakin ingin hapus buku ${title} ?`);
 }
 
 
-
-
 let totalCount = 0;
-let totalPrice = 0
+let totalPrice = 0;
+let book = [];
+
 $('#jumlah').text(totalCount);
 $('#total-price').html(`<label class="badge text-bg-success">Rp. ${totalPrice}</label>`);
+
 $('input[type="checkbox"]').on('change', function() {
 
   const price = $(this).data('price');
-  const curretRow = $(this).closest("tr");
-  const getcount = curretRow.find('td:eq(3)').text();
-  const count = parseInt(getcount);
+  const currentRow = $(this).closest("tr");
+  const getCount = currentRow.find('td:eq(3)').text();
+  const count = parseInt(getCount);
+  const title = currentRow.find('td:eq(0)').text();
 
+  let checkLength = $('input[type="checkbox"]:checked').length;
   if (this.checked) {
     totalCount = totalCount + count;
+    // sampai sini
+    const title_count = {
+      
+    }
+    book.push(title)
   } else {
     totalCount = totalCount - count;
+    const index = book.indexOf(title);
+    if (index > -1) { // only splice array when item is found
+      book.splice(index, 1); // 2nd parameter means remove one item only
+    }
   }
-  totalPrice = totalCount*price
+
+  totalPrice = totalCount * price;
+  if (checkLength > 0 && $('.button-pay').length === 0) {
+    $('#form-data').append('<button type="button" class="btn btn-primary btn-sm button-pay" id="pay-now">Pay Now</button>');
+  } else if (checkLength === 0) {
+    $('.button-pay').remove();
+  }
 
   $('#jumlah').text(totalCount);
   $('#total-price').html(`<label class="badge text-bg-success">Rp. ${totalPrice}</label>`);
-  console.log(totalCount);
+
+  $("#pay-now").on("click", ()=>{
+    console.log()
+  })
 });
+
 
